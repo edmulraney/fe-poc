cd1c5bfaed94ba3e7f68dda1071f356ba19f5384
Edward Mulraney <edmulraney@gmail.com>
2019-04-10 10:14:57 +0100

    initial commit

---
+++ script-compiled.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var _fePocRegistry = require("fe-poc-registry");
+
+console.log('calling dashboard');
+(0, _fePocRegistry.dashboard)();
---
+++ src/index.js
@@ -0,0 +1,6 @@
+// import { dashboard } from 'fe-poc-registry'
+
+// console.log('calling dashboard')
+// dashboard()
+
+const tt = require('fe-poc-registry')
